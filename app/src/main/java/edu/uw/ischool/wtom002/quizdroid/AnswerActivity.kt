1e0d417e69e001daeb467e480df7dd5105c9f936
William Tom <wtom002@uw.edu>
2023-11-01 01:11:34 -0700

    bugs

--- app/src/main/java/edu/uw/ischool/wtom002/quizdroid/AnswerActivity.kt
+++ app/src/main/java/edu/uw/ischool/wtom002/quizdroid/AnswerActivity.kt
@@ -1,86 +1,43 @@
 package edu.uw.ischool.wtom002.quizdroid
 
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-
-import android.content.Context
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import java.io.BufferedReader
-import java.io.InputStreamReader
-
-data class QuizQuestion(
-    val question: String,
-    val options: List<String>,
-    val correctAnswer: String
-)
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_answer.*
 
 class AnswerActivity : AppCompatActivity() {
-
-    private var currentQuestionIndex = 0
-
-    private val quizQuestions = loadQuizQuestions(this, "quiz_questions.json")
-
-
-    fun loadQuizQuestions(context: Context, jsonFileName: String): List<QuizQuestion> {
-        val json: String
-        val typeToken = object : TypeToken<List<QuizQuestion>>() {}.type
-
-        try {
-            val inputStream = context.assets.open(jsonFileName)
-            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-            json = bufferedReader.use { it.readText() }
-        } catch (ex: Exception) {
-            ex.printStackTrace()
-            return emptyList()
-        }
-
-        return Gson().fromJson(json, typeToken)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
         setContentView(R.layout.activity_answer)
 
-        val selectedAnswerTextView = findViewById<TextView>(R.id.userAnswerTextView)
-        val correctAnswerTextView = findViewById<TextView>(R.id.correctAnswerTextView)
-        val progressTextView = findViewById<TextView>(R.id.progressTextView)
-        val nextButton = findViewById<Button>(R.id.nextButton)
-        val finishButton = findViewById<Button>(R.id.finishButton)
+        val selectedAnswerIndex = intent.getIntExtra("selectedAnswerIndex", -1)
+        val correctAnswerIndex = intent.getIntExtra("correctAnswerIndex", -1)
+        val totalCorrect = intent.getIntExtra("totalCorrect", 0)
+        val totalQuestions = intent.getIntExtra("totalQuestions", 0)
 
-        val selectedAnswer = intent.getStringExtra("selected_answer")
-        val correctAnswer = "Correct Answer" // Replace with the actual correct answer
-        val isLastQuestion = false // Replace with logic to determine if it's the last question
+        answerGivenTextView.text = "Your answer: ${selectedAnswerIndex + 1}"
+        correctAnswerTextView.text = "Correct answer: ${correctAnswerIndex + 1}"
+        correctnessTextView.text = "You have $totalCorrect out of $totalQuestions correct"
 
-        selectedAnswerTextView.text = "Your Answer: $selectedAnswer"
-        correctAnswerTextView.text = "Correct Answer: $correctAnswer"
-
-        // Update the progressTextView with correct vs. incorrect answers
-
-        if (isLastQuestion) {
-            nextButton.visibility = View.GONE
-            finishButton.visibility = View.VISIBLE
-        } else {
-            nextButton.visibility = View.VISIBLE
-            finishButton.visibility = View.GONE
-        }
-
-        nextButton.setOnClickListener {
+        if (totalCorrect < totalQuestions) {
+            // If there are more questions remaining, show the "Next" button.
             nextButton.setOnClickListener {
-                if (currentQuestionIndex < quizQuestions.size - 1) {
-                    // There are more questions
-                    currentQuestionIndex++
-                    updateUIWithQuestion(quizQuestions[currentQuestionIndex])
-        }
+                val intent = Intent(this, QuestionActivity::class.java)
+                intent.putExtra("totalCorrect", totalCorrect)
+                intent.putExtra("totalQuestions", totalQuestions)
+                startActivity(intent)
+                finish()
+            }
 
-        finishButton.setOnClickListener {
-            // Handle navigation back to the topic list page
+            finishButton.visibility = View.GONE
+        } else {
+            // If all questions are answered, show the "Finish" button.
+            nextButton.visibility = View.GONE
+            finishButton.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
---
+++ app/src/main/java/edu/uw/ischool/wtom002/quizdroid/QuestionActivity.kt
@@ -0,0 +1,73 @@
+package edu.uw.ischool.wtom002.quizdroid
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+data class Question(
+    val questionText: String,
+    val options: List<String>,
+    val correctAnswerIndex: Int
+)
+class QuestionActivity : AppCompatActivity() {
+    private val questions = mutableListOf(
+        Question("What is the capital of France?", listOf("Paris", "Berlin", "Madrid", "London"), 0),
+        Question("Which planet is known as the Red Planet?", listOf("Mars", "Venus", "Jupiter", "Saturn"), 0),
+    )
+
+    private var currentQuestionIndex = 0
+    private var totalCorrect = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+
+        updateUI()
+
+        submitButton.setOnClickListener {
+            val selectedRadioButtonId = radioGroup.checkedRadioButtonId
+            if (selectedRadioButtonId != -1) {
+                val selectedAnswerIndex = when (selectedRadioButtonId) {
+                    R.id.radioButton1 -> 0
+                    R.id.radioButton2 -> 1
+                    R.id.radioButton3 -> 2
+                    R.id.radioButton4 -> 3
+                    else -> -1
+                }
+
+                val correctAnswerIndex = questions[currentQuestionIndex].correctAnswerIndex
+
+                if (selectedAnswerIndex == correctAnswerIndex) {
+                    totalCorrect++
+                }
+
+                if (currentQuestionIndex < questions.size - 1) {
+                    currentQuestionIndex++
+                    updateUI()
+                    radioGroup.clearCheck()
+                } else {
+                    val intent = Intent(this, AnswerActivity::class.java)
+                    intent.putExtra("totalCorrect", totalCorrect)
+                    intent.putExtra("totalQuestions", questions.size)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+
+    private fun updateUI() {
+        val currentQuestion = questions[currentQuestionIndex]
+        questionTextView.text = currentQuestion.questionText
+        radioButton1.text = currentQuestion.options[0]
+        radioButton2.text = currentQuestion.options[1]
+        radioButton3.text = currentQuestion.options[2]
+        radioButton4.text = currentQuestion.options[3]
+    }
+}
+
+
+
